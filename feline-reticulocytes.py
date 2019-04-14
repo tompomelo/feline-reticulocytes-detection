786ecf986666f7895ee69327776d213a5aa5283c
Tom Still <tom.still@live.co.uk>
2019-04-14 19:47:46 +0100

    Created .py version for Spyder IDE

---
+++ feline-reticulocytes.py
@@ -0,0 +1,29 @@
+#Copy for Spyder IDE (Anaconda)
+
+# install / update keras, tensorflow and theano 
+
+#(pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git)
+#(pip install --upgrade keras)
+#(pip install tensorflow)
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Conv2D(32, (3, 3) input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Flatten())
+
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
